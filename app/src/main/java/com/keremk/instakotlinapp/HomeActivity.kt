934ac6c7fc60d2d2511b3fb94571f31d27a4712d
Kerem Karataş <keremkaratas420@gmail.com>
2020-06-17 22:26:27 +0300

    inital commit

---
+++ app/src/main/java/com/keremk/instakotlinapp/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.keremk.instakotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
